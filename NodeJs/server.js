19cc7788c4c98f45cfe278b927dfbe8b0f1e08c9
Felipe Alves <felipealvesinfor@gmail.com>
2020-10-28 16:38:40 -0300

    configuring Mongoose

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 
+// Iniciando o App
 const app = express();
 
+// Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi')
+
+// Primeira rota
 app.get('/', (req, res) => {
   res.send("Hello Felipe");
 })
