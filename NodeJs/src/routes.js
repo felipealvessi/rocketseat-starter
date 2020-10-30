a58f35ac990ce76453905a4be75e761e54380b85
Felipe Alves <felipealvesinfor@gmail.com>
2020-10-30 09:56:59 -0300

    refactoring

--- NodeJs/src/routes.js
+++ NodeJs/src/routes.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  Product.create({
-    title: "React Native",
-    description: "Build native apps with React",
-    url: "https://github.com/facebook/react-native"
-  });
+const ProductController = require('./controllers/ProductController');
 
-  return res.send("Hello Felipe");
-})
+routes.get('/products', ProductController);
+
 
 module.exports = routes;
