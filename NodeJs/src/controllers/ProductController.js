9e0b275bbc7b943fde439aba2db70d246edd1ab1
Felipe Alves <felipealvesinfor@gmail.com>
2020-10-30 10:03:18 -0300

    refactoring

--- NodeJs/src/controllers/ProductController.js
+++ NodeJs/src/controllers/ProductController.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
 module.exports = {
-  async indexedDB(req, res) {
+  async index(req, res) {
     const products = await Product.find();
 
     return res.json(products);
